e04f8f0a4192a8e92608660eac95c2dad90184fe
developer.niroj <developer.niroj@gmail.com>
2022-05-16 01:33:04 +0545

    shared images folder added in git

--- signalR/Controllers/testController.cs
+++ signalR/Controllers/testController.cs
@@ -25,7 +25,7 @@ namespace signalR.Controllers
 
                 foreach (string u_name in globalVariables.user_name)
                 {
-                    if (u_name != user_name && u_name!=null)
+                    if (u_name != user_name && u_name != null)
                     {
                         u.Add(new userModel
                         {
@@ -48,41 +48,41 @@ namespace signalR.Controllers
             try
             {
 
-            string file_path = "";
-            var fileContent = model.ImageFile;
-            if (fileContent != null && fileContent.ContentLength > 0)
-            {
-                string path = System.Web.HttpContext.Current.Server.MapPath("~/shared images/");
-                //check for extensions
-                var allowedExtensions = new[] { ".jpg", ".jpeg", ".png", ".tiff", ".bmp" };
-                var extension = Path.GetExtension(fileContent.FileName);
-                if (allowedExtensions.Contains(extension))
+                string file_path = "";
+                var fileContent = model.ImageFile;
+                if (fileContent != null && fileContent.ContentLength > 0)
                 {
+                    string path = System.Web.HttpContext.Current.Server.MapPath("~/shared images/");
+                    //check for extensions
+                    var allowedExtensions = new[] { ".jpg", ".jpeg", ".png", ".tiff", ".bmp" };
+                    var extension = Path.GetExtension(fileContent.FileName);
+                    if (allowedExtensions.Contains(extension))
+                    {
                         string return_file_path = Path.GetFileNameWithoutExtension(fileContent.FileName) + Guid.NewGuid() + extension;
-                    string actual_path = path + return_file_path;
-                        
+                        string actual_path = path + return_file_path;
+
                         //check if file is already present
                         if (!System.IO.File.Exists(fileContent.FileName))
                         {
                             fileContent.SaveAs(actual_path);
                         }
-                    file_path = return_file_path;
-                }
+                        file_path = return_file_path;
+                    }
                     else
                     {
                         return Json(new { success = false, responseText = "Only image files are allowed" });
                     }
-            }
-            else
-            {
-                return Json(new { success = false, responseText = "Please upload an image to transfer." });
-            }
-            chatHub.send_files(model.message_from, model.message_to, file_path);
+                }
+                else
+                {
+                    return Json(new { success = false, responseText = "Please upload an image to transfer." });
+                }
+                chatHub.send_files(model.message_from, model.message_to, file_path);
                 var result = new { success = true, responseText = "File send successful", file_path = file_path };
                 return Json(result);
 
             }
-            catch (Exception)
+            catch (Exception ex)
             {
                 return Json(new { success = false, responseText = "Failed to transfer file to" + model.message_to });
             }
